21ce6d5a60c84e767c63738660faaa91e170d57c
arunavnarayan <58374569+arunavnarayan@users.noreply.github.com>
2021-05-24 00:23:56 +0530

    db change

--- db_conn.py
+++ db_conn.py
@@ -9,9 +9,9 @@ import pymysql
 def connect_to_db():
     connection = pymysql.connect(host='sql228.main-hosting.eu',
                                  port=3306,
-                                 user='u474015652_arunav03',
-                                 password='03@arunav',
-                                 db='u474015652_demo1',
+                                 user='****',
+                                 password='***',
+                                 db='***',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
     return connection
